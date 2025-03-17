6919517369780fa4e8d0b152278bd2f265572161
veit1337 <veit.gemmer@gmail.com>
2025-03-17 19:52:34 +0100

    refactor: add MuscleLengthData to interface

--- dev/models/muscle_tendon_muscle_model_interface.py
+++ dev/models/muscle_tendon_muscle_model_interface.py
@@ -3,8 +3,13 @@ from __future__ import annotations
 from abc import ABC
 from abc import abstractmethod
 
+from dev.data.muscle_length_data import MuscleLengthData
+
 
 class MuscleTendonMuscleModelInterface(ABC):
     @abstractmethod
-    def simulate_forward_step(self):
+    def simulate_forward_step(
+            self, muscle_length_datas:
+            tuple[MuscleLengthData, MuscleLengthData]) \
+            -> tuple[MuscleLengthData, MuscleLengthData]:
         pass
