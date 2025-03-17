fe39f3118e6c775041da22b40b967a782b0b28fc
veit1337 <veit.gemmer@gmail.com>
2025-03-17 20:23:31 +0100

    refactor: remove MuscleLengthData from Interface

--- dev/models/muscle_tendon_muscle_model_interface.py
+++ dev/models/muscle_tendon_muscle_model_interface.py
@@ -3,13 +3,9 @@ from __future__ import annotations
 from abc import ABC
 from abc import abstractmethod
 
-from dev.data.muscle_length_data import MuscleLengthData
-
 
 class MuscleTendonMuscleModelInterface(ABC):
     @abstractmethod
-    def simulate_forward_step(
-            self, muscle_length_datas:
-            tuple[MuscleLengthData, MuscleLengthData]) \
-            -> tuple[MuscleLengthData, MuscleLengthData]:
+    def simulate_forward_step(self, muscle_length_one: float,
+                              muscle_length_two: float) -> float:
         pass
