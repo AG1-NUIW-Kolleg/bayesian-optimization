d1d17204a8c9b1990a949c80af839ee321523e70
veit1337 <veit.gemmer@gmail.com>
2025-03-19 13:18:12 +0100

    docs: add comments for units

--- dev/constants/physical.py
+++ dev/constants/physical.py
@@ -1,3 +1,3 @@
 from __future__ import annotations
-RELAXED_MUSCLE_LENGTH_ONE = 9.0
-RELAXED_MUSCLE_LENGTH_TWO = 12.0
+RELAXED_MUSCLE_LENGTH_ONE = 9.0  # in cm
+RELAXED_MUSCLE_LENGTH_TWO = 12.0  # in cm
