2d45226508e673e1514528d95defc45bc7b83471
Klara Gutekunst <klara.gutekunst.kg@gmail.com>
2025-03-19 12:11:15 +0100

    style: Add commentary

--- test.py
+++ test.py
@@ -1,11 +1,12 @@
 from __future__ import annotations
 
 from dev.models.hill_type_model_wrapper import HillTypeModelWrapper
+# slack muscle length
 params = {
     'Length_Slack_M1': 13,
     'Length_Slack_M2': 13,
 }
 model = HillTypeModelWrapper(params)
 
-range_of_motion = model.simulate_forward_step(13.0, 13.0)
+range_of_motion = model.simulate_forward_step(3.0, 3.0) # prestrech muscle length
 print(range_of_motion)
