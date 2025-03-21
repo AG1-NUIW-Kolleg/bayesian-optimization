abb64a2c043ffb8cdfe0a41a253ea3f56656b0aa
veit1337 <veit.gemmer@gmail.com>
2025-03-21 15:48:58 +0100

    style: make name of muscle_length more descriptive

--- dev/visual/range_of_motion_plotter.py
+++ dev/visual/range_of_motion_plotter.py
@@ -10,13 +10,13 @@ from dev.constants.physical import RELAXED_MUSCLE_LENGTH_TWO
 
 class RangeOfMotionPlotter():
     def __init__(self, initial_muscle_lengths, range_of_motions):
-        self._muscle_lengths = initial_muscle_lengths
+        self._stretched_muscle_lengths = initial_muscle_lengths
         self._range_of_motions = range_of_motions
 
     def save_as_csv(self):
         range_of_motions = self._range_of_motions.flatten()
-        stretched_muscle_length_one = self._muscle_lengths[:, 0]
-        stretched_muscle_length_two = self._muscle_lengths[:, 1]
+        stretched_muscle_length_one = self._stretched_muscle_lengths[:, 0]
+        stretched_muscle_length_two = self._stretched_muscle_lengths[:, 1]
 
         df_len = len(range_of_motions)
         relaxed_muscle_length_one = np.full(df_len, RELAXED_MUSCLE_LENGTH_ONE)
@@ -35,8 +35,8 @@ class RangeOfMotionPlotter():
         fig = plt.figure()
         ax = fig.add_subplot(111, projection='3d')
 
-        x = self._muscle_lengths[:, 0]
-        y = self._muscle_lengths[:, 1]
+        x = self._stretched_muscle_lengths[:, 0]
+        y = self._stretched_muscle_lengths[:, 1]
         z = self._range_of_motions.flatten()
 
         sc = ax.scatter(x, y, z, c=z)
