dc29d1446c3333e343cbe7272a55d825ba7932d1
veit1337 <veit.gemmer@gmail.com>
2025-09-15 11:05:30 +0200

    feat: adjust params to Hill Type model

--- dev/constants/bayes.py
+++ dev/constants/bayes.py
@@ -1,9 +1,9 @@
 from __future__ import annotations
 # boundaries for bayes optimization
-MIN_STRETCHED_MUSCLE_LENGTH_ONE = 9.0  # in cm
-MAX_STRETCHED_MUSCLE_LENGTH_ONE = 18.0  # in cm
-MIN_STRETCHED_MUSCLE_LENGTH_TWO = 12.0  # in cm
-MAX_STRETCHED_MUSCLE_LENGTH_TWO = 21.0  # in cm
+MIN_STRETCHED_MUSCLE_LENGTH_ONE = 12.0  # in cm
+MAX_STRETCHED_MUSCLE_LENGTH_ONE = 17.0  # in cm
+MIN_STRETCHED_MUSCLE_LENGTH_TWO = 13.0  # in cm
+MAX_STRETCHED_MUSCLE_LENGTH_TWO = 18.0  # in cm
 
 # Bayesian Optimization
 SEED = 37
