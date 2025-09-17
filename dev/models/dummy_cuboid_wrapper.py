d1882551f16ec11cf5a82f5875b5a1e1cb8fb438
veit1337 <veit.gemmer@gmail.com>
2025-09-17 10:57:30 +0200

    refactor(DummyCuboidWrapper): hand environment to subprocess
    To be honest, I don't know how this code works exactly. I will try to
    find out afterwards :)

--- dev/models/dummy_cuboid_wrapper.py
+++ dev/models/dummy_cuboid_wrapper.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 import subprocess
 import sys
+import os
 
 
 class DummyCuboidWrapper():
@@ -10,10 +11,15 @@ class DummyCuboidWrapper():
         self._parser = parser
         self._params = params
 
+        self._project_root = \
+            os.path.dirname(os.path.dirname(os.path.dirname(
+                simulation_script_path)))
+
     def simulate_dummy_step(self):
-        # run dummy model script here
+        env = os.environ.copy()
+        env['PYTHONPATH'] = self._project_root
         subprocess.run([sys.executable, self._simulation_script_path],
-                       check=True)
+                       check=True, env=env)
 
         range_of_motion = self._parser.parse_range_of_motion()
 
