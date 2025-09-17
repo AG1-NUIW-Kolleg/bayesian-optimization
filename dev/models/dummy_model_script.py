bd84517d1db80a731ba1c316e7bfc7abcf4d7085
veit1337 <veit.gemmer@gmail.com>
2025-09-17 11:02:13 +0200

    style(DummyModelScript): remove whitespace

--- dev/models/dummy_model_script.py
+++ dev/models/dummy_model_script.py
@@ -7,6 +7,5 @@ from dev.constants import FILEPATH_OUTPUT
 sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(
     os.path.abspath(__file__)))))
 
-
 with open(FILEPATH_OUTPUT, 'w') as file:
     file.write('187')
