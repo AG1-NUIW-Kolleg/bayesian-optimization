ae5ff53c269c13555b3f0b0b28610dd08e70b924
veit1337 <veit.gemmer@gmail.com>
2025-09-18 10:07:33 +0200

    refactor(RangeOfMotionParser): adapt parser to cuboid output

--- dev/util/range_of_motion_parser.py
+++ dev/util/range_of_motion_parser.py
@@ -3,7 +3,10 @@ from __future__ import annotations
 import os
 import sys
 
+import pandas as pd
+
 from dev.constants import FILEPATH_OUTPUT
+
 sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(
     os.path.abspath(__file__)))))
 
@@ -13,8 +16,14 @@ class RangeOfMotionParser():
         self._filepath = filepath
 
     def parse_range_of_motion(self):
-        range_of_motion = -1
         with open(self._filepath) as file:
-            range_of_motion = file.read()
+            content = file.read()
+            values = content.split(',')
+            values = [float(x) for x in values if len(x) > 0]
+
+        df = pd.DataFrame(values)
+        min_length = df.min()[0]
+        max_length = df.max()[0]
+        range_of_motion = max_length - min_length
 
         return range_of_motion
